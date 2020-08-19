f398c1557e9e3bf546b64e53d3b2a73905fa7955
nescafecat <cdxrcq@gmail.com>
2020-08-19 14:42:58 +0800

    202008191442

--- shoesl.c
+++ shoesl.c
@@ -2,13 +2,17 @@
 #define ADJUST 7.31
 int main(void)
 {
-    consst double SCALE = 0.333;
+    const double SCALE = 0.333;
     double shoe, foot;
 
-    shoe = 9.0;
-    foot = SCALE * shoe + ADJUST;
     printf("Shoe size (men's)   foot length\n");
-    printf("%10.1f %15.2f inches\n", shoe, foot);
-
+    shoe = 3.0;
+    while (shoe < 18.5)
+    {
+        foot = SCALE * shoe + ADJUST;
+        printf("%10.1f %15.2f inches\n", shoe, foot);
+        shoe = shoe + 1.0;
+    }
+    printf("if the shoe fits, weart.\n");
     return 0;
 }
